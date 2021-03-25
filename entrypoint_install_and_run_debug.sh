9361c56b4987f7b14b94a43fce0596557b9a8a2d
dezhidki <dezhidki@jyu.fi>
2021-03-25 18:52:53 +0200

    Update chmods in entrypoint scripts

--- entrypoint_install_and_run_debug.sh
+++ entrypoint_install_and_run_debug.sh
@@ -6,7 +6,7 @@ xdebug.client_host = host.docker.internal\n\
 xdebug.mode = debug\n\
 xdebug.start_with_request = yes\n\' >>/usr/local/etc/php/php.ini
 
-sed -i '3ichmod a+rwx /var/data/api/stack /var/data/api/stack/logs /var/data/api/stack/tmp /var/data/api/stack/plots' /usr/local/bin/docker-php-entrypoint
+sed -i '3ichmod a=rwx /var/data/api/stack /var/data/api/stack/logs /var/data/api/stack/tmp /var/data/api/stack/plots' /usr/local/bin/docker-php-entrypoint
 echo "<?php phpinfo(); ?>" >phpinfo.php
 . /etc/apache2/envvars
 apache2 -D FOREGROUND
